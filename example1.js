148241dfbf52b5e39db3fc31b8abf3adc2b81864
inc-dev <107142790+inc-dev@users.noreply.github.com>
2023-03-12 22:04:32 +0300

    comment

---
+++ example1.js
@@ -0,0 +1,12 @@
+ app.draw = function(){
+    var ctx = this.ctx; 
+    ctx.save(); 
+    ctx.scale(this.canvas_scale,this.canvas_scale);     
+    ctx.fillStyle = "#244";
+    ctx.fillRect(0, 0, this.canvas_width+1, this.canvas_height+1);                               
+    ctx.fillStyle = "rgb(255,165,0)";
+    ctx.font = '18px Arial ';
+    ctx.fillText("Hello world "+window.devicePixelRatio, 10, 30);
+    ctx.restore(); 
+   }  
+
