59ae004dea93995a0339782711dc475841176579
inc-dev <107142790+inc-dev@users.noreply.github.com>
2023-03-14 00:12:44 +0300

    comment

--- example1.js
+++ example1.js
@@ -2,7 +2,7 @@
     var ctx = this.ctx; 
     ctx.save(); 
     ctx.scale(this.canvas_scale,this.canvas_scale);     
-    ctx.fillStyle = "#244";
+    ctx.fillStyle = "#444";
     ctx.fillRect(0, 0, this.canvas_width+1, this.canvas_height+1);                               
     ctx.fillStyle = "rgb(255,165,0)";
     ctx.font = '18px Arial ';
