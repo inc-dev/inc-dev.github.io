92fd0f69a480f7a8fb4b0dd0a70b53b7f4b18541
inc-dev <107142790+inc-dev@users.noreply.github.com>
2023-03-13 23:59:55 +0300

    events

--- example1.js
+++ example1.js
@@ -6,7 +6,7 @@
     ctx.fillRect(0, 0, this.canvas_width+1, this.canvas_height+1);                               
     ctx.fillStyle = "rgb(255,165,0)";
     ctx.font = '18px Arial ';
-    ctx.fillText("Hello world "+window.devicePixelRatio, 10, 30);
+    ctx.fillText("Hello world "+(window.devicePixelRatio*100)+"% "+this.last_event, 10, 30);
     ctx.restore(); 
    }  
 
