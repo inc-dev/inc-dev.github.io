24cc61934314de1a471c88af750f223657df1eec
inc-dev <107142790+inc-dev@users.noreply.github.com>
2023-03-14 00:49:31 +0300

    comment

--- example1.js
+++ example1.js
@@ -2,11 +2,26 @@
     var ctx = this.ctx; 
     ctx.save(); 
     ctx.scale(this.canvas_scale,this.canvas_scale);     
-    ctx.fillStyle = "#444";
-    ctx.fillRect(0, 0, this.canvas_width+1, this.canvas_height+1);                               
-    ctx.fillStyle = "rgb(255,165,0)";
+    ctx.fillStyle = "#08090d";
+    ctx.fillRect(0, 0, this.canvas_scale_width+1, this.canvas_scale_height+1);
+    ctx.fillStyle = "#0d1015";
+    ctx.fillRect(10, 10, 700, 200);
+    
+    ctx.lineWidth = this.canvas_scale;
+    ctx.strokeStyle = "#2d566c";
+    ctx.beginPath();
+    ctx.rect(10, 10, 700, 200);
+    ctx.stroke();
+
+    ctx.strokeStyle = "#27384c";
+    ctx.beginPath();
+    ctx.moveTo(15, 45);
+    ctx.lineTo(700-5, 45);
+    ctx.stroke();
+    //ctx.fillStyle = "rgb(255,165,0)";
+    ctx.fillStyle = "#6fd594";
     ctx.font = '18px Arial ';
-    ctx.fillText("Hello world "+(window.devicePixelRatio*100)+"% "+this.last_event, 10, 30);
+    ctx.fillText("Hello world "+(window.devicePixelRatio*100)+"% "+this.last_event, 20, 40);
     ctx.restore(); 
    }  
 
