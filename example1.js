90f2209725870e2cedaaa9e146c773cbbd768954
inc-dev <107142790+inc-dev@users.noreply.github.com>
2023-03-14 22:44:30 +0300

    comment

--- example1.js
+++ example1.js
@@ -1,13 +1,21 @@
  app.draw = function(){
+    var width = this.canvas_width;
+    var height = this.canvas_height;
+    var lineWidth = 1;
     var ctx = this.ctx; 
     ctx.save(); 
+
     ctx.scale(this.canvas_scale,this.canvas_scale);     
+    width = this.canvas_scale_width;
+    height = this.canvas_scale_height;
+    lineWidth = this.canvas_scale
+
     ctx.fillStyle = "#08090d";
-    ctx.fillRect(0, 0, this.canvas_scale_width+1, this.canvas_scale_height+1);
+    ctx.fillRect(0, 0, width+1, height+1);
     ctx.fillStyle = "#0d1015";
     ctx.fillRect(10.5, 10.5, 700, 200);
     
-    ctx.lineWidth = this.canvas_scale;
+    ctx.lineWidth = lineWidth;
     ctx.strokeStyle = "#2d566c";
     ctx.beginPath();
     ctx.rect(10.5, 10.5, 700, 200);
