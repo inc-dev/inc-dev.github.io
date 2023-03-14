0bf2d868223c85c7d3ebf2e846437e7b472e0c06
Mankov <inc_dev@mail.ru>
2023-03-14 14:02:49 +0300

    comment

--- example1.js
+++ example1.js
@@ -5,18 +5,18 @@
     ctx.fillStyle = "#08090d";
     ctx.fillRect(0, 0, this.canvas_scale_width+1, this.canvas_scale_height+1);
     ctx.fillStyle = "#0d1015";
-    ctx.fillRect(10, 10, 700, 200);
+    ctx.fillRect(10.5, 10.5, 700, 200);
     
     ctx.lineWidth = this.canvas_scale;
     ctx.strokeStyle = "#2d566c";
     ctx.beginPath();
-    ctx.rect(10, 10, 700, 200);
+    ctx.rect(10.5, 10.5, 700, 200);
     ctx.stroke();
 
     ctx.strokeStyle = "#27384c";
     ctx.beginPath();
-    ctx.moveTo(15, 45);
-    ctx.lineTo(700-5, 45);
+    ctx.moveTo(15.5, 45.5);
+    ctx.lineTo(700-5.5, 45.5);
     ctx.stroke();
     //ctx.fillStyle = "rgb(255,165,0)";
     ctx.fillStyle = "#6fd594";
