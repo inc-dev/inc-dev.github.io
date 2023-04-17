5adfe5148ae348db2d047789228c7dbc9fdf62fe
inc-dev <107142790+inc-dev@users.noreply.github.com>
2023-04-17 22:07:01 +0300

    comment

--- example1.js
+++ example1.js
@@ -1,24 +1,25 @@
  app.draw = function(){
     var width = this.canvas_width;
     var height = this.canvas_height;
-    var lineWidth = 1;
+    var lineWidth = 2;//this.canvas_scale
     var ctx = this.ctx; 
+    var shift = 0; //0.5 0
     ctx.save(); 
 
-    ctx.scale(this.canvas_scale,this.canvas_scale);     
+//    ctx.scale(this.canvas_scale,this.canvas_scale);     
     width = this.canvas_scale_width;
     height = this.canvas_scale_height;
-    lineWidth = this.canvas_scale
 
-    ctx.fillStyle = "#08090d";
+
+    ctx.fillStyle = "#080808";
     ctx.fillRect(0, 0, width+1, height+1);
     ctx.fillStyle = "#0d1015";
-    ctx.fillRect(10.5, 10.5, 700, 200);
+    ctx.fillRect(10+shift, 10+shift, 700, 200);
     
     ctx.lineWidth = lineWidth;
     ctx.strokeStyle = "#2d566c";
     ctx.beginPath();
-    ctx.rect(10.5, 10.5, 700, 200);
+    ctx.rect(10+shift, 10+shift, 700, 200);
     ctx.stroke();
 
     ctx.strokeStyle = "#27384c";
@@ -28,7 +29,7 @@
     ctx.stroke();
     //ctx.fillStyle = "rgb(255,165,0)";
     ctx.fillStyle = "#6fd594";
-    ctx.font = '18px Arial ';
+    ctx.font = 'normal 300 32px roboto';//Arial roboto
     ctx.fillText("Hello world "+(window.devicePixelRatio*100)+"% "+this.last_event, 20, 40);
     ctx.restore(); 
    }  
